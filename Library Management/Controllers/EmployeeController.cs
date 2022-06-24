ffaea369e8172ac61399a8736ddedc2fe040e54f
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-06-24 19:40:29 +0400

    feat: added EmployeeController

---
+++ Library Management/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using Library_Management.DAL;
+using Library_Management.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library_Management.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly Context _context;
+
+        public EmployeeController(Context context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Employee> employees = await _context.Employees.ToListAsync();
+            return View(employees);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Employee employee)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _context.AddAsync(employee);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
