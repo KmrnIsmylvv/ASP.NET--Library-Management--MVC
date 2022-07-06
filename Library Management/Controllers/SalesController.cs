57ecd285a09c222d899267ea4c2356b7f0ec795f
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-07-06 10:27:33 +0400

    feat: Lending Books

---
+++ Library Management/Controllers/SalesController.cs
@@ -0,0 +1,36 @@
+﻿using Library_Management.DAL;
+using Library_Management.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management.Controllers
+{
+    public class SalesController : Controller
+    {
+        private readonly Context _context;
+
+        public SalesController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Lend()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Lend(Sales sales)
+        {
+            _context.Sales.Add(sales);
+            _context.SaveChanges();
+            return RedirectToAction("Lend");
+
+
+        }
+    }
+}
