ff630ead92e1e29038e484997e7179fd47ed67c1
DESKTOP-50U56HO <kamranvi@code.edu.az>
2022-07-18 10:53:13 +0400

    feat: Added Statistics page

---
+++ Library Management/Controllers/StatisticsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Library_Management.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
